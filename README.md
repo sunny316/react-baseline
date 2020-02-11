fb9d07549ebcd2594ff32d8f6422ebce11d9ae77
n0g006r <nikhil.garg@walmart.com>
2020-02-11 15:05:27 +0530

    Added build script and manifest.json

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-baseline
 React setup with minimal packages
+
+To install the packages : npm install
+
+To start the server : npm run dev
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const WebpackAssetsManifest = require('webpack-assets-manifest');
 var parentDir = path.join(__dirname, './');
 
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: [
         path.join(parentDir, 'index.js')
     ],
@@ -23,12 +23,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: parentDir,
-        historyApiFallback: true,
-        port: 4001,
-        hot: true
     },
     plugins: [
         new WebpackAssetsManifest({
